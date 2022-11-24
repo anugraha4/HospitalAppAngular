4c4b6e5e60c1f86d51eb9b285c0edc8c93864042
anugraha4 <96745531+anugraha4@users.noreply.github.com>
2022-11-24 14:10:40 +0530

    view all

---
+++ src/app/view-all-patients/view-all-patients.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-all-patients',
+  templateUrl: './view-all-patients.component.html',
+  styleUrls: ['./view-all-patients.component.css']
+})
+export class ViewAllPatientsComponent {
+
+}
